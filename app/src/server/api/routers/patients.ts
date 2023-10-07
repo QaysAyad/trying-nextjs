a4d6d1fa51660712eeabd0b139e10953450a994e
Qays Al-Keero <55286922+QaysAyad@users.noreply.github.com>
2023-10-07 12:53:06 +0200

    Added getById

--- app/src/server/api/routers/patients.ts
+++ app/src/server/api/routers/patients.ts
@@ -6,6 +6,11 @@ import {
 } from "~/server/api/trpc";
 
 export const patientsRouter = createTRPCRouter({
+  getById: protectedProcedure
+    .input(z.object({ id: z.number() }))
+    .query(({ ctx, input }) => {
+      return ctx.db.patient.findFirst({ where: { id: input.id } });
+    }),
   search: protectedProcedure
     .input(z.object({ client_id: z.string() }))
     .query(({ ctx, input }) => {
