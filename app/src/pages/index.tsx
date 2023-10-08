199c375299eecbe6f68d9718a2063a5eba3be89e
Qays Al-Keero <55286922+QaysAyad@users.noreply.github.com>
2023-10-08 21:30:12 +0200

    Updated HomePage

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -2,11 +2,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 
-import { api } from "~/utils/api";
-
 export default function Home() {
-  // const hello = api.patient.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Head>
@@ -14,15 +10,12 @@ export default function Home() {
         <meta name="description" content="Know Patents App" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Welcome to <span className="text-[hsl(280,100%,70%)]">Know Patents</span> App
           </h1>
           <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {/* {hello.data ? hello.data.greeting : "Loading tRPC query..."} */}
-            </p>
             <AuthShowcase />
           </div>
         </div>
@@ -33,22 +26,41 @@ export default function Home() {
 
 function AuthShowcase() {
   const { data: sessionData } = useSession();
-  // const { data: secretMessage } = api.patient.getSecretMessage.useQuery(
-  //   undefined, // no input
-  //   { enabled: sessionData?.user !== undefined }
-  // );
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {/* {secretMessage && <span> - {secretMessage}</span>} */}
+        {sessionData && <span>Logged in as {sessionData.user?.email}</span>}
       </p>
+      {sessionData && <PagesLinks/>}
+      <br />
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
         onClick={sessionData ? () => void signOut() : () => void signIn()}
       >
         {sessionData ? "Sign out" : "Sign in"}
       </button>
+    </div>
+  );
+}
+
+function PagesLinks() {
+  const links = [
+    { href: "/patients", text: "Patients" },
+    { href: "/compare", text: "Compare Patients" },
+  ];
+  return (
+    <div className="flex flex-col items-center justify-center w-80 gap-4 text-white">
+      <br />
+      We have two pages for you to explore
+      {links.map((link) => (
+        <Link
+          key={link.href}
+          href={link.href}
+          className="flex flex-col items-center justify-center rounded-lg text-sm h-12 font-semibold w-full bg-white/10 text-white hover:bg-slate-700"
+        >
+          {link.text}
+        </Link>
+      ))}
     </div>
   );
 }
