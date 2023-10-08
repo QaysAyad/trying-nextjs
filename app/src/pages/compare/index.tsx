166fbd1d8b23fffc4e7629d71386372a2155ee1b
Qays Al-Keero <55286922+QaysAyad@users.noreply.github.com>
2023-10-08 22:32:04 +0200

    Fixed imports

--- app/src/pages/compare/index.tsx
+++ app/src/pages/compare/index.tsx
@@ -4,7 +4,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import { use, useEffect, useMemo, useState } from "react";
-import { Chart, ChartDataPoints } from "~/components/chart";
+import { Chart, type ChartDataPoints } from "~/components/Chart";
 
 import { api } from "~/utils/api";
 
@@ -24,9 +24,9 @@ export default function Compare() {
   );
 }
 function AuthPart() {
-  const { data: sessionData } = useSession();
   const [search, setSearch] = useState('');
-  const { data: patients } = api.patients.search.useQuery({ client_id: search });
+  const { data: sessionData } = useSession();
+  const { data: patients, isLoading: isLoadingPatients } = api.patients.search.useQuery({ client_id: search });
   const [_selectedPatients, setSelectedPatients] = useState(new Map<Patient['id'], Patient>());
   const selectedPatients = useMemo(() => Array.from(_selectedPatients), [_selectedPatients]);
 
@@ -34,28 +34,30 @@ function AuthPart() {
 
   // if (!sessionData) return <p>Access Denied</p>
 
-
-  const searchInput = <input
-    placeholder="Search with Client ID"
-    className="grow bg-transparent outline-none"
-    type="text"
-    onChange={(e) => setSearch(e.target.value)}
-  />;
-  if (!patients) return <>{searchInput}<div>Loading...... </div></>;
   return (
     <>
-      {searchInput}
-      <div>
-        {patients.map((patient) =>
-          <button
-            className="bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-            key={patient.id}
-            onClick={() =>
-              setSelectedPatients((prev) => new Map(prev).set(patient.id, patient))}
-          >
-            {patient.client_id}
-          </button>)}
-        <br />
+      <div className="flex">
+        <div className="flex flex-col">
+          <h2 className="text-3xl font-semibold text-white">Patients</h2>
+          <input
+            placeholder="Search with Client ID"
+            className="grow bg-transparent outline-none"
+            type="text"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <div className="w-50 h-40  overflow-scroll flex flex-col items-stretch">
+            {isLoadingPatients && <div>Loading...</div>}
+            {patients?.map((patient) =>
+              <button
+                className="bg-white/10 px-2 font-semibold text-white no-underline transition hover:bg-white/20"
+                key={patient.id}
+                onClick={() =>
+                  setSelectedPatients((prev) => new Map(prev).set(patient.id, patient))}
+              >
+                {patient.client_id}
+              </button>)}
+          </div>
+        </div>
         <SelectedBox
           data={selectedPatients}
           onRemove={(id) => setSelectedPatients((prev) => {
@@ -68,20 +70,22 @@ function AuthPart() {
       <ChartBox data={selectedPatients} />
     </>
   );
-
-
 }
 
 function SelectedBox({ data, onRemove }: { data: [Patient['id'], Patient][], onRemove: (id: Patient['id']) => void }) {
-  return <div className="border">
-    {data.map(([id, patient]) =>
-      <button
-        className="bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        key={id}
-        onClick={() => onRemove(id)}
-      >
-        {patient.client_id} RemoveIcon
-      </button>)}
+  return <div>
+    <h2 className="text-3xl font-semibold text-white">Selected</h2>
+    <div className="border">
+      {data.map(([id, patient]) =>
+        <button
+          className="bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+          key={id}
+          onClick={() => onRemove(id)}
+        >
+          {patient.client_id} RemoveIcon
+        </button>)}
+    </div>
+
   </div>;
 }
 
