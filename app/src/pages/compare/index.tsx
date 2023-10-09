6e02a27bb3ce1d12b03b4a3de37db59e79ce653b
Qays Al-Keero <55286922+QaysAyad@users.noreply.github.com>
2023-10-09 07:16:45 +0200

    Added table to compare page

--- app/src/pages/compare/index.tsx
+++ app/src/pages/compare/index.tsx
@@ -1,10 +1,14 @@
-import type { Patient } from "@prisma/client";
+import type { DataPoint, Patient } from "@prisma/client";
 import { useMemo, useState } from "react";
 import { api } from "~/utils/api";
 import HeadAndBackground from "~/components/HeadAndBackground";
 import { MeasurementChart, measurementKeys } from "~/components/MeasurementChart";
 import AuthRenderProtector from "~/components/AuthRenderProtector";
 import Loading from "~/components/loading";
+import { Table, TableBody, TableDataCell, TableHeader, TableHeaderColumnCell, TableHeaderRowCell, TableRow, TableRowEvenOdd } from "~/components/Table";
+import { dayFormatter } from "~/utils/dayjs";
+import { snakeCaseToText } from "~/utils/string";
+import { RejectButton } from "~/components/Buttons";
 
 export default function Compare() {
 
@@ -20,11 +24,13 @@ export default function Compare() {
     </>
   );
 }
+
+type SelectedPatients = [Patient['id'], Patient][];
 function AuthPart() {
   const [search, setSearch] = useState('');
   const { data: patients, isLoading: isLoadingPatients } = api.patients.search.useQuery({ client_id: search });
   const [_selectedPatients, setSelectedPatients] = useState(new Map<Patient['id'], Patient>());
-  const selectedPatients = useMemo(() => Array.from(_selectedPatients), [_selectedPatients]);
+  const selectedPatients = useMemo<SelectedPatients>(() => Array.from(_selectedPatients), [_selectedPatients]);
   return (
     <>
       <div className="flex">
@@ -49,21 +55,17 @@ function AuthPart() {
               </button>)}
           </div>
         </div>
-        <SelectedBox
-          data={selectedPatients}
-          onRemove={(id) => setSelectedPatients((prev) => {
-            const newMap = new Map(prev);
-            newMap.delete(id);
-            return newMap;
-          })}
-        />
       </div>
-      {selectedPatients.length && <AllCharts data={selectedPatients} />}
+      {selectedPatients.length && <AllCharts deselect={(id) => setSelectedPatients((prev) => {
+        const newMap = new Map(prev);
+        newMap.delete(id);
+        return newMap;
+      })} data={selectedPatients} />}
     </>
   );
 }
 
-function SelectedBox({ data, onRemove }: { data: [Patient['id'], Patient][], onRemove: (id: Patient['id']) => void }) {
+function SelectedBox({ data, onRemove }: { data: SelectedPatients, onRemove: (id: Patient['id']) => void }) {
   return <div>
     <h2 className="text-3xl font-semibold text-white">Selected</h2>
     <div className="border">
@@ -80,10 +82,84 @@ function SelectedBox({ data, onRemove }: { data: [Patient['id'], Patient][], onR
   </div>;
 }
 
-export function AllCharts({ data }: { data: [Patient['id'], Patient][] }) {
+type PatentsDataPoints = Map<Patient['id'], DataPoint[]>;
+export function AllCharts({ data, deselect }: { data: SelectedPatients, deselect: DataTableProps['deselect'] }) {
   const { data: dataPoints } = api.dataPoints.getAllForPatients.useQuery({ patient_ids: data.map(([id]) => id) });
   if (!dataPoints) return <Loading />;
   return <div className="flex flex-col items-center">
+    <div className="w-full">
+      {dataPoints && <DataTable deselect={deselect} selectedPatients={data} data={dataPoints} />}
+    </div>
     {measurementKeys.map((key) => <MeasurementChart key={key} measurementKey={key} data={dataPoints} />)}
   </div>;
+}
+
+interface DataTableProps {
+  selectedPatients: SelectedPatients;
+  data: PatentsDataPoints;
+  deselect(id: Patient['id']): void;
+}
+
+function DataTable({ selectedPatients, data, deselect }: DataTableProps) {
+  return <Table>
+    <TableHeader>
+      <TableRow>
+        <TableHeaderColumnCell>
+          Client ID
+        </TableHeaderColumnCell>
+        <TableHeaderColumnCell>
+          Birth Date
+        </TableHeaderColumnCell>
+        <TableHeaderColumnCell>
+          Ethnicity
+        </TableHeaderColumnCell>
+        <TableHeaderColumnCell>
+          Gender
+        </TableHeaderColumnCell>
+        <TableHeaderColumnCell>
+          Last Testing Date
+        </TableHeaderColumnCell>
+        {measurementKeys.map((key) => <TableHeaderColumnCell key={key}>
+          {snakeCaseToText(key)}
+        </TableHeaderColumnCell>)}
+        <TableHeaderColumnCell>
+          Action
+        </TableHeaderColumnCell>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      {[...data.keys()].map((id, i) => {
+        const patient = selectedPatients.find(([_, p])=>id===p.id)![1];
+        const dataPoint = data.get(id)!.slice(-1)[0]!;
+        return <TableRowEvenOdd key={i} even={!(i % 2)}>
+          <TableHeaderRowCell>
+            {patient.client_id}
+          </TableHeaderRowCell>
+          <TableDataCell>
+            {dayFormatter(patient.date_birthdate)}
+          </TableDataCell>
+          <TableDataCell>
+            {/* TODO: Map the enum numbers to text readable values */}
+            {patient.ethnicity}
+          </TableDataCell>
+          <TableDataCell>
+            {/* TODO: Map the enum numbers to text readable values */}
+            {patient.gender}
+          </TableDataCell>
+          <TableDataCell>
+            {/* We shouldn't use utc because it should show the day of the testing corresponding to the system date. */}
+            {dayFormatter(dataPoint.date_testing)}
+          </TableDataCell>
+          {measurementKeys.map((key) => <TableDataCell key={key}>
+            {dataPoint[key]}
+          </TableDataCell>)}
+          <TableDataCell>
+            <RejectButton onClick={() => deselect(id)}>
+              Deselect
+            </RejectButton>
+          </TableDataCell>
+        </TableRowEvenOdd>
+      })}
+    </TableBody>
+  </Table>;
 }
