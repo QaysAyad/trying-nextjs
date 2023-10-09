e195a966380dcb950bfb273e5bd8e075ef209147
Qays Al-Keero <55286922+QaysAyad@users.noreply.github.com>
2023-10-09 05:03:59 +0200

    Added Table to Patient Page

--- app/src/pages/patients/[client_id].tsx
+++ app/src/pages/patients/[client_id].tsx
@@ -5,6 +5,10 @@ import { ssgHelper } from "~/helpers/ssg";
 import { api } from "~/utils/api";
 import AuthRenderProtector from "~/components/AuthRenderProtector";
 import Loading from "~/components/loading";
+import { Suspense } from "react";
+import type { DataPoint } from "@prisma/client";
+import dayjs from "dayjs";
+import { snakeCaseToText } from "~/utils/string";
 
 // TODO: Remove this.
 // Patient.getInitialProps = (ctx: NextPageContext) => {
@@ -14,30 +18,75 @@ import Loading from "~/components/loading";
 //   return { client_id: ctx.query.client_id }
 // }
 
-interface Props { client_id: string  }
+interface Props { client_id: string }
 const Patient: NextPage<Props> = ({ client_id }) => {
   const { data: patientData, isLoading } = api.patients.getPublic.useQuery({ client_id });
   if (isLoading) return <Loading />;
   if (!patientData) return <div>404</div>;
-
-  const { data, isLoading: isLoadingData } =
-    api.patients.getByClientIdWithDataPoints.useQuery({ client_id });
-
   return (
     <HeadAndBackground
       title={`Know Patents App - ${patientData.client_id}`}
       content={`Patent ${patientData.client_id} page`}
       meta={[{ name: "description", content: `Patent ${patientData.client_id} info` }]}
     >
-      <AuthRenderProtector>
-        {isLoadingData && <Loading />}
-        {data && <AllMeasurementsCharts data={new Map([[patientData.id, data.dataPoints]])} />}
-      </AuthRenderProtector>
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-center text-2xl text-white">
+          <span>Patient: {patientData.client_id}</span>
+        </h1>
+        <br />
+        <AuthRenderProtector>
+          <Suspense fallback={<Loading />}>
+            <AuthPart client_id={client_id} />
+          </Suspense>
+        </AuthRenderProtector>
+      </div>
     </HeadAndBackground>
   );
 }
 
 export default Patient;
+
+
+function AuthPart({ client_id }: Props) {
+  const { data, isLoading: isLoadingData } =
+    api.patients.getByClientIdWithDataPoints.useQuery({ client_id });
+  return (
+    <>
+      {isLoadingData && <Loading />}
+      {data && <Table data={data.dataPoints} />}
+      {data && <AllMeasurementsCharts data={new Map([[data.id, data.dataPoints]])} />}
+    </>
+  );
+}
+
+function Table({ data }: { data: DataPoint[] }) {
+  return <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+      <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <tr>
+          <th scope="col" className="px-6 py-3">
+            Testing Date
+          </th>
+          {measurementKeys.map((key) => <th key={key} scope="col" className="px-6 py-3">
+            {snakeCaseToText(key)}
+          </th>)}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((dataPoint, i) =>
+          <tr key={i} className={`border-b dark:border-gray-700 ${i % 2 ? 'bg-white dark:bg-gray-900' : 'bg-gray-50 dark:bg-gray-800'}`}>
+            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              {/* We shouldn't use utc because it should show the day of the testing corresponding to the system date. */}
+              {dayjs(dataPoint.date_testing).format("YYYY-MM-DD")}
+            </th>
+            {measurementKeys.map((key) => <td key={key} className="px-6 py-4">
+              {dataPoint[key]}
+            </td>)}
+          </tr>)}
+      </tbody>
+    </table>
+  </div>
+}
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const client_id = context.params?.client_id;
